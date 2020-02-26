28d825605e536992773d7ff25b849b2fb851164b
Heaciy <duke3247561384@icloud.com>
2020-02-26 17:51:03 +0800

    add AddRoute()

--- Homework/TakeBus/map.cpp
+++ Homework/TakeBus/map.cpp
@@ -20,6 +20,12 @@ void LoadMapData() {
         g_sMap.stations[i].station = STATION[i]; // 初始化站点名
         g_sMap.stations[i].routes = NULL; // 下行路线暂时设置为空
     }
+    for (int i = 0; i < BUS_NUM; i++) {
+        AddBus(BUSES[i][0], BUSES[i][1], BUSES[i][2]);
+    }
+    for (int i = 0; i < BUS_NUM; i++) {
+        AddRoute(ROUTES[i][0],ROUTES[i][1],ROUTES[i][2],ROUTES[i][3]);
+    }
 }
 
 int FindBus(string bus) {
@@ -58,16 +64,45 @@ int GetStation(string station) {
     if (nStation == -1) {
         g_sMap.stations = (Station *) realloc(g_sMap.stations, sizeof(Station) * g_sMap.station_num + 1);
         Station *pStation = g_sMap.stations + g_sMap.station_num;
-        pStation->station=station;
-        pStation->routes=NULL;
+        pStation->station = station;
+        pStation->routes = NULL;
         g_sMap.station_num++;
     }
     return nStation;
 }
 
-void AddBus(string bus, string pStart, string pEnd){
-    int nBus=GetBus(bus);
-    int nStart=GetStation(pStart);
+void AddBus(string bus, string pStart, string pEnd) {
+    int nBus = GetBus(bus);
+    int nStart = GetStation(pStart);
     int nEnd = GetStation(pEnd);
-    Bus*pBus=g_sMap.buses+nBus;
+    Bus *pBus = g_sMap.buses + nBus;
+    pBus->start = nStart;
+    pBus->end = nEnd;
+}
+
+bool AddRoute(string pBus, string pStart, string pEnd, int distance) {
+    int nBus = GetBus(pBus);
+    int nStart = GetStation(pStart);
+    int nEnd = GetStation(pEnd);
+    Station *pStStation = &g_sMap.stations[nStart];
+    Route *pStRoute = pStStation->routes; // 数组的第一个route
+    while (pStRoute != NULL && pStRoute->next != NULL) {
+        if (pStRoute->bus == nBus && pStRoute->station == nEnd) {
+            return false;
+        }
+        pStRoute = pStRoute->next; // 起始点相同的route(属于不同路线)
+    }
+    // 未找到与之匹配的,则创建新的路线
+    Route *pNewRoute = (Route *) malloc(sizeof(Route));
+    pNewRoute->bus = nBus;
+    pNewRoute->station = nEnd;
+    pNewRoute->distance = distance;
+    pNewRoute->next = NULL;
+    //若是起始顶点的第一条边
+    if (pStRoute == NULL) {
+        pStStation->routes = pNewRoute;
+    } else {
+        pStRoute->next = pNewRoute;
+    }
+    return true;
 }
